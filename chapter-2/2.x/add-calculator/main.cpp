ca410f9a5ede8cdea773098230338bf7015b70dd
heyset <c.inaciomatheus@gmail.com>
2021-11-21 17:47:31 -0300

    Adds write function

---
+++ chapter-2/2.x/add-calculator/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+void write(std::string output)
+{
+    std::cout << output;
+}
+
+int main()
+{
+    write("Vamo nessa");
+}
+
+
+// read
+// addTwoIntegers
+// write
+
